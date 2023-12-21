177b76a488f68d499a67551db41f9b04a110ba5e
root <dkk@aju.co.kr>
2023-12-21 13:14:05 +0900

    webtoon study finish

---
+++ Webtoon/models/webtoon_episode_model.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class WebtoonEpisodeModel {
+  final String id, title, rating, date;
+
+  WebtoonEpisodeModel.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        title = json['title'],
+        rating = json['rating'],
+        date = json['date'];
+}
